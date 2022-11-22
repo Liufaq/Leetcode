49efe338be7a665d9e5091f0b31b3f49d06af77e
Gq <1611266263@qq.com>
2022-11-22 13:45:20 +0800

    第 n 个神奇数字

--- src/NthMagicalNumber.java
+++ src/NthMagicalNumber.java
@@ -17,4 +17,35 @@
         链接：https://leetcode.cn/problems/nth-magical-number
         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 public class NthMagicalNumber {
+    static final int MOD = 1000000007;
+    public int Res(int n,int a,int b){
+        long l = Math.min(a, b);//左边界
+        long r = (long) n * Math.min(a, b); // 右边界
+        int c = lcm(a, b); //最小公倍数
+        while (l <= r) {//二分查找
+            long mid = (l + r) / 2;
+            long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int) ((r + 1) % MOD);
+    }
+
+
+
+
+    //最大公约数
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    //最小公倍数
+    int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+
 }
